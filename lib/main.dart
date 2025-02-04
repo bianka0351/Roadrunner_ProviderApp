4d3b7f117b61f583b682abc787d8297ffcd83dcb
bianka hmmo <biankahmmo@gmail.com>
2025-02-04 16:34:42 +0300

    Adding the modified files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:roadrunner_provider_app/features/auth/bloc/bloc/auth_bloc.dart';
+import 'package:roadrunner_provider_app/features/auth/presentation/screens/splash_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +14,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return BlocProvider<AuthBloc>(
+      create: (context) => AuthBloc(),
+      child: ScreenUtilInit(
+          designSize: const Size(428, 926), // الحجم المرجعي للتصميم
+
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            builder: BotToastInit(),
+            home: SplashScreen(),
+            // routes: {'PhoneNumberPage': (context) => PhoneNumberPage()},
+          )),
     );
   }
 }
