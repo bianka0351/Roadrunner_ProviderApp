6edc7c9a449ff1eecc9535f080539022eef9be26
azigTovmasian <azig.tovmasian7@gmail.com>
2025-02-24 11:13:39 +0300

    files: removed all other user story files

--- lib/features/profile/buisness_logic/bloc/profile_event.dart
+++
@@ -1,9 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-abstract class ProfileEvent extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
-
-// fetch profile data from api
-class FetchProfile extends ProfileEvent {}
